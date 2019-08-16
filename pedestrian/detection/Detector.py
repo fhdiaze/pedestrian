b26378f096c1a40b6b4ada0eb1f5d8b6b8df2b94
Fredy Diaz <fhdiaz87@gmail.com>
2019-08-15 23:32:19 -0500

    Testing tracking

--- pedestrian/detection/Detector.py
+++ pedestrian/detection/Detector.py
@@ -6,7 +6,7 @@ class Detector(object):
     def detect(self, frame):
         """Finds pedestrians in a frame
 
-        :param np.ndarray frame: (h, w, 3)
+        :param np.ndarray frame: (h, w, [R, G, B])
         :return: (samples, [x1, y1, x2, y2, score])
         :rtype: np.ndarray
         """
--- tests/TestTracking.py
+++ tests/TestTracking.py
@@ -11,7 +11,7 @@ from pedestrian.position.TwoCornersPM import TwoCornersPM
 from pedestrian.tracking.Sort import Sort
 
 # Pipeline Variables
-det_period = 30
+det_period = 10
 outline = "blue"
 proto = "C:/Users/kuby/Downloads/object-detection-deep-learning/MobileNetSSD_deploy.prototxt.txt"
 model = "C:/Users/kuby/Downloads/object-detection-deep-learning/MobileNetSSD_deploy.caffemodel"
@@ -26,9 +26,10 @@ in_path = "C:/Users/kuby/Downloads/"
 # out_path = "/home/investigacion/Downloads/"
 out_path = "C:/Users/kuby/Downloads/"
 in_name = "Ch4_20181029060955.mp4"
-out_name = "Box_" + os.path.splitext(in_name)[0] + ".wmv"
+out_name = "Box_" + os.path.splitext(in_name)[0] + ".avi"
 in_video = os.path.join(in_path, in_name)
 out_video = os.path.join(out_path, out_name)
+out_fps = 30
 
 COLORS = np.random.uniform(0, 255, size=(100, 3))
 
@@ -37,28 +38,26 @@ cap = cv2.VideoCapture(os.path.join(in_path, in_name))
 video_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 video_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 t_range = np.array([[0.0, video_width], [0.0, video_height]])
-out = cv2.VideoWriter(out_video, cv2. VideoWriter_fourcc(*"MJPG"), 5, (video_width, video_height))
+out = cv2.VideoWriter(out_video, cv2. VideoWriter_fourcc(*"MJPG"), out_fps, (video_width, video_height))
 
 f = 0
-while cap.isOpened():
-    print(f)
+while cap.isOpened() and f < 1000:
     ret, bgr_frame = cap.read()
-    rgb_frame = cv2.cvtColor(bgr_frame, cv2.COLOR_BGR2RGB)
 
     if ret:
         boxes = np.empty((0, 0))
 
         if f % det_period == 0:
+            rgb_frame = cv2.cvtColor(bgr_frame, cv2.COLOR_BGR2RGB)
             boxes = detector.detect(rgb_frame)
 
         tracks = tracker.track(boxes)
 
-        for track in tracks:
+        for track in boxes:
             (x1, y1, x2, y2, idx) = track
-            cv2.rectangle(bgr_frame, (x1, y1), (x2, y2), COLORS[idx], 2)
+            cv2.rectangle(bgr_frame, (x1, y1), (x2, y2), COLORS[1], 2)
 
-        box_bgr_frame = cv2.cvtColor(bgr_frame, cv2.COLOR_RGB2BGR)
-        out.write(box_bgr_frame)
+        out.write(bgr_frame)
 
     f += 1
 
